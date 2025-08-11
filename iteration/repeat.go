4e3db6e96d4e3bd4edb67854d5b329c72e12bcad
Jonathan Hou <jhou20022004@gmail.com>
2025-08-11 10:31:21 -0400

    Added parameter to adjust repeat count

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -4,9 +4,9 @@ import "strings"
 
 const repeatCount = 5
 
-func Repeat(character string) string {
+func Repeat(character string, num int) string {
 	var repeated strings.Builder
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < num; i++ {
 		repeated.WriteString(character)
 	}
 
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,16 +3,27 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat 5 times", func(t * testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+	
+	t.Run("repeat 1 time", func(t *testing.T) {
+		repeated := Repeat("a", 1)
+		expected := "a"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-        Repeat("a")
+        Repeat("a", 1)
     }
 }
