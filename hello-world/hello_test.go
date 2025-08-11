a09f6465115d4a655ee6eea73503d97bf281dfa2
Jonathan Hou <jhou20022004@gmail.com>
2025-08-11 00:24:33 -0400

    Added language functionality to hello.go and tests

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,15 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Jonathan")
+		got := Hello("Jonathan", "English")
 		want := "Hello, Jonathan"
 		
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Spy", "French") 
+		want := "Bonjour, Spy"
 
 		assertCorrectMessage(t, got, want)
 	})
